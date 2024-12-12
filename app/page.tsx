c4449704f8aeb5ddcd405abd749221b773173a3a
trace2798 <113078518+trace2798@users.noreply.github.com>
2024-12-12 21:40:53 +0530

    connected through dashboard test

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hello Test with workers
+      Hello Test with workers. COnnected this with github repo through dashboard
     </main>
   );
 }

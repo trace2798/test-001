e68368a7690e1f9f78864d1be4d8f7f763e7acd3
trace2798 <113078518+trace2798@users.noreply.github.com>
2024-12-13 15:19:09 +0530

    test

--- components/editor.tsx
+++ components/editor.tsx
@@ -1,14 +1,14 @@
 "use client";
 
-import { useEditor, EditorContent } from "@tiptap/react";
-import StarterKit from "@tiptap/starter-kit";
-import { FC } from "react";
-import TaskItem from "@tiptap/extension-task-item";
-import TaskList from "@tiptap/extension-task-list";
 import Table from "@tiptap/extension-table";
 import TableCell from "@tiptap/extension-table-cell";
 import TableHeader from "@tiptap/extension-table-header";
 import TableRow from "@tiptap/extension-table-row";
+import TaskItem from "@tiptap/extension-task-item";
+import TaskList from "@tiptap/extension-task-list";
+import { EditorContent, useEditor } from "@tiptap/react";
+import StarterKit from "@tiptap/starter-kit";
+import { FC } from "react";
 
 interface EditorProps {}
 
